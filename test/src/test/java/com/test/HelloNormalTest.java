620efb1d3ca14e870a78af2d5ddcb0b79903d361
王飞 <fei.wang@fengjr.com>
2020-11-17 13:25:58 +0800

    a

--- test/src/test/java/com/test/HelloNormalTest.java
+++ test/src/test/java/com/test/HelloNormalTest.java
@@ -1,8 +1,37 @@
 package com.test;
 
+import demo.Hello;
+
+import java.lang.management.ManagementFactory;
+
 public class HelloNormalTest {
 
     public static void main(String[] args) {
+        int i = 0;
+        try {
+            while (true) {
+                System.out.println("pid = [" + getPid() + "]");
+                i++;
+                Hello hello = new Hello();
+                hello.sayA("lhy");
+                Thread.sleep(10000l);
+            System.out.println("-------------continue----------------");
 
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getPid() {
+        String pid = null;
+        try {
+            Class.forName("sun.jvmstat.monitor.MonitoredHost");
+            String name = ManagementFactory.getRuntimeMXBean().getName();
+            pid = name.split("@")[0];
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return pid;
     }
 }
--- test/src/test/java/com/test/HelloWithAnnotationTest.java
+++ test/src/test/java/com/test/HelloWithAnnotationTest.java
@@ -1,14 +1,15 @@
 package com.test;
 
 import com.wangf.agent.javaassist.VmAttachHelper2;
-import demo.Hello;
 
 public class HelloWithAnnotationTest {
     public static void main(String[] args) {
         String agentPath = "/Users/wangfei/Devlop/openSource/agent-study/annotation-agent/target/annotation-agent.jar";
-        VmAttachHelper2.attach(agentPath);
-        Hello hello=new Hello();
-        String name="wf";
-        hello.sayA(name);
+//        String pid="9405";
+        String pid=null;
+        VmAttachHelper2.attach(pid,agentPath);
+//        Hello hello=new Hello();
+//        String name="wf";
+//        hello.sayA(name);
     }
 }
